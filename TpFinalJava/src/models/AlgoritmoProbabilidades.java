413fe8987da1afe7cd62d96cb2bcce50135a12b8
UnderABloodySky <valenzuelahoracioe@gmail.com>
2018-05-20 23:09:36 -0300

    Agrego las clases relacionadas a la CasaDeApuestas

---
+++ TpFinalJava/src/models/AlgoritmoProbabilidades.java
@@ -0,0 +1,7 @@
+package models;
+
+public interface AlgoritmoProbabilidades {
+	
+	public Float calcularProbabilidades(Partido _partido);
+
+}
---
+++ TpFinalJava/src/models/CompetenciaHistoricaDirecta.java
@@ -0,0 +1,12 @@
+package models;
+
+public class CompetenciaHistoricaDirecta implements AlgoritmoProbabilidades {
+
+	@Override
+	public Float calcularProbabilidades(Partido _partido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
