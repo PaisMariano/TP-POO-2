fb7db02d2750b3e447a5f647565659033ffbe79b
UnderABloodySky <valenzuelahoracioe@gmail.com>
2018-05-28 00:50:00 -0300

    Modificaciones en Segura y Final

--- Proyecto/src/apuesta/Final.java
+++ Proyecto/src/apuesta/Final.java
@@ -2,18 +2,13 @@ package apuesta;
 
 public class Final extends TipoApuesta {
 
-	private Exception errorCancelar() {
-		return new Exception("La apuesta no puede ser cancelada");
-	}
-
 	@Override
 	protected Boolean puedeSerCancelada(Apuesta _apuesta) {
-		Boolean no = new Boolean(false); 
-		return no;
+		return new Boolean(false); 
 	}
 
 	@Override
 	protected void cancelarApuesta(Apuesta _apuesta) {
-		this.errorCancelar();
+		//No deberia de llegar aca.
 	}
 }
