d6f1089c9433983b56fc418bd2e6c99bfd2eaeb2
UnderABloodySky <valenzuelahoracioe@gmail.com>
2018-05-28 14:03:18 -0300

    Resultado

--- Proyecto/src/resultados/Resultado.java
+++ Proyecto/src/resultados/Resultado.java
@@ -1,16 +1,9 @@
 package resultados;
 
-import identificables.*;
 import oponentes.Oponente;
 
-public abstract class Resultado implements Identificable{
+public abstract class Resultado{
 	
 		public abstract Oponente getGanador();
-
-		@Override
-		public Boolean es(Object _object) {
-			return new Boolean (this.hashCode() == _object.hashCode());
-		}
-
 
 }
