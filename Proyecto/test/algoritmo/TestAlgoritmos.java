79acb35b9bc02e153bfe99d3576fef498183744b
Mariano Pais <mariano_a_p@hotmail.com>
2018-05-31 00:37:17 -0400

    Agregue tests clase TestAlgoritmos

--- Proyecto/test/algoritmo/TestAlgoritmos.java
+++ Proyecto/test/algoritmo/TestAlgoritmos.java
@@ -1,25 +1,65 @@
 package algoritmo;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import apuesta.Apuesta;
 import eventoDeportivo.EventoDeportivo;
+import oponentes.Deportista;
+import oponentes.Oponente;
 import eventoDeportivo.Deporte;
 import org.junit.Before;
 import org.junit.Test;
 import estado.EstadoEventoDeportivo;
 import static org.mockito.Mockito.*;
 
-class TestAlgoritmos {
+import java.util.List;
+import java.util.ArrayList;
 
+public class TestAlgoritmos {
+	
+	private AlgoritmoProbabilidades algPro;
+	private EventoDeportivo evento;
+	private Oponente dummyOp1;
+	private Oponente dummyOp2;
+	private List<EventoDeportivo> historico;
+		
 	@Before
-	void setUp() throws Exception {
+	public void setUp() throws Exception {
+		//SetUp
+		algPro = new CompetenciaHistoricaDirecta();
+		
+		dummyOp1 = mock(Deportista.class);
+		dummyOp2 = mock(Deportista.class);
+		evento = mock(EventoDeportivo.class);
+		
+		historico = new ArrayList<EventoDeportivo>();
+		
+		historico.add(evento);
+		historico.add(evento);
+		historico.add(evento);
+		historico.add(evento);
+	}
+
+	
+	@Test
+	public void testCalcularHistoricoEntreTeniendoCuatroIteraciones() {
+		
+		when(evento.participaronVs(dummyOp1, dummyOp2)).thenReturn(true);
+		
+		algPro.calcularHistoricoEntre(historico, dummyOp1, dummyOp2);
+		
+		verify(evento, times(4)).participaronVs(dummyOp1, dummyOp2);
+		
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testVerificoQueLaProbabilidadDelOponenteUnoSeaCuatro() {
+		
+		when(evento.getGanador()).thenReturn(dummyOp1);
+		
+		assertEquals(algPro.probabilidadGanador(historico, dummyOp1),new Float(4));
+		
 	}
-
-
+	
 }
